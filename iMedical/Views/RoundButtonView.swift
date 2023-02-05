78d77b89f783953b76272af459cd5a8cd10f5f70
iosBlessed <kolobanov.nikita.99@gmail.com>
2023-02-05 00:23:01 +0000

    Added function that simulates tap on button

--- iMedical/Views/RoundButtonView.swift
+++ iMedical/Views/RoundButtonView.swift
@@ -28,4 +28,28 @@ class RoundButtonView: UIButton {
             self.layer.borderColor = borderColor.cgColor
         }
     }
+    
+    func initializeBackgroundView(){
+        
+        self.borderColor = UIColor(displayP3Red: 0.294, green: 0.608, blue: 0.647, alpha: 0.8)
+        self.borderWidth = 2
+        self.cornerRadius = 20
+        self.tintColor = UIColor(white: 1.0, alpha: 0.8)
+        self.backgroundColor = UIColor(red: 48.0 / 255.0, green: 43.0 / 255.0, blue: 77.0 / 255.0, alpha: 1.0)
+        self.layer.shadowColor = UIColor(displayP3Red: 0.353, green: 0.757, blue: 0.816, alpha: 0.8).cgColor
+        self.layer.shadowRadius = 10
+        self.layer.shadowOffset = .zero
+        self.layer.shadowOpacity = 1.0
+        
+    }
+    
+    func tapSimulator(){
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            self.layer.shadowOpacity = 0.0
+        }, completion: {_ in
+            self.layer.shadowOpacity = 1.0
+        })
+        
+    }
 }
